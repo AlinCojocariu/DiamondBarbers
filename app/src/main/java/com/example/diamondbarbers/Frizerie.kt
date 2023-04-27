b12ad9c129853fcd00f3b38a4e36150cd3ff5de8
alinc <alincojocariu00@gmail.com>
2023-04-27 22:39:03 +0300

    Added HairStylistActivity with the list of hairstylists. Each profile picture is taken from Firebase Storage

--- app/src/main/java/com/example/diamondbarbers/Frizerie.kt
+++ app/src/main/java/com/example/diamondbarbers/Frizerie.kt
@@ -1,4 +1,40 @@
 package com.example.diamondbarbers
 
-class Frizerie(var nume:String,var adresa:String,var oras:String,var hairStylists: List<HairStylist>) {
+import android.os.Parcel
+import android.os.Parcelable
+
+class Frizerie(
+    var nume: String,
+    var adresa: String,
+    var oras: String,
+    var hairStylists: List<HairStylist>
+) : Parcelable {
+
+    constructor(parcel: Parcel) : this(
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.createTypedArrayList(HairStylist.CREATOR)!!
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(nume)
+        parcel.writeString(adresa)
+        parcel.writeString(oras)
+        parcel.writeTypedList(hairStylists)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Frizerie> {
+        override fun createFromParcel(parcel: Parcel): Frizerie {
+            return Frizerie(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Frizerie?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
