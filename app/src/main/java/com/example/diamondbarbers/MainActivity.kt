9afaaf0f5a34fe2d222af9cfab9eeb57f5f4903f
alinc <alincojocariu00@gmail.com>
2023-05-03 20:13:05 +0300

    start sms verification

--- app/src/main/java/com/example/diamondbarbers/MainActivity.kt
+++ app/src/main/java/com/example/diamondbarbers/MainActivity.kt
@@ -1,78 +1,144 @@
 package com.example.diamondbarbers
 
 
+import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseTooManyRequestsException
+import com.google.firebase.auth.*
 import com.google.firebase.database.*
+import com.google.firebase.ktx.Firebase
+import java.util.concurrent.TimeUnit
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var numeClient: EditText
     private lateinit var nrTelefon: EditText
-    private lateinit var salveaza: Button
+    private lateinit var introducereCod: EditText
+    private lateinit var sendcode: Button
+    private lateinit var verifycode: Button
     private lateinit var databaseRef: DatabaseReference
+    private lateinit var auth:FirebaseAuth
+    private var storedVerificationId: String?= ""
+    private lateinit var resedToken:PhoneAuthProvider.ForceResendingToken
+    private lateinit var callbacks:PhoneAuthProvider.OnVerificationStateChangedCallbacks
 
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+      //  FirebaseApp.initializeApp(this)
         numeClient = findViewById(R.id.numeClient)
         nrTelefon = findViewById(R.id.nrTelefon)
-        salveaza = findViewById(R.id.Salveaza)
+        introducereCod = findViewById(R.id.introducereCod)
+        sendcode = findViewById(R.id.trimitereCod)
+        verifycode=findViewById(R.id.verificareCod)
         databaseRef = FirebaseDatabase.getInstance().getReference("clients")
-        salveaza.setOnClickListener {
-            saveClient()
+
+            auth = Firebase.auth
+        
+        sendcode.setOnClickListener {
+            startPhoneNumberVerification(nrTelefon!!.text.toString())
+
 
         }
-    }
 
-    private fun saveClient() {
-        val name = numeClient.text.toString().trim()
-        val phone = nrTelefon.text.toString().trim()
-
-        if (name.isEmpty()) {
-            numeClient.error = "insert you'r name"
-            return
+        verifycode.setOnClickListener{
+            verifyPhoneNumberWithCode(storedVerificationId,introducereCod!!.text.toString())
         }
 
-        if (phone.isEmpty()) {
-            nrTelefon.error = "insert phone number"
-            return
-        }
 
-        if (name.isNotEmpty() && phone.isNotEmpty()) {
 
-            val newRef = databaseRef.push()
-            newRef.setValue(User(name, phone)).addOnCompleteListener {
-                val userId = newRef.key
-               // Toast.makeText(applicationContext, userId, Toast.LENGTH_LONG).show()
+
+
+        callbacks =  object  :  PhoneAuthProvider.OnVerificationStateChangedCallbacks(){
+            override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+                Log.d(TAG, "onVerificationCompleted:$credential")
+                signInWithPhoneAuthCredential(credential)
             }
 
-            val intent = Intent(this, ListaFrizerii::class.java)
+            override fun onVerificationFailed(e: FirebaseException) {
+                Log.w(TAG,"onVerificationFailed",e)
 
-            val bundle = Bundle()
-            bundle.putString("phone", phone)
-
-
-            intent.putExtras(bundle)
-
-            startActivity(intent)
+                if(e is FirebaseAuthInvalidCredentialsException)
+                {}
+                else if(e is FirebaseTooManyRequestsException)
+                {}
 
         }
-//        //val clientId = databaseRef.push().key!!
-//
-//        val client = Client("4",name, phone)
-//        databaseRef.child("4").setValue(client).addOnCompleteListener{
-//            Toast.makeText(this,"salvat cu succes",Toast.LENGTH_LONG).show()
-//        }
-//            .addOnFailureListener{ err->
-//                Toast.makeText(this,"eroare ${err.message}",Toast.LENGTH_LONG).show()
-//            }
+
+            override fun onCodeSent(verificationId: String, token: PhoneAuthProvider.ForceResendingToken) {
+
+                Log.d(TAG,"onCodeSent:$verificationId")
+                storedVerificationId= verificationId
+                resedToken= token
+            }
+
+            }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser=auth.currentUser
+        updateUI(currentUser)
 
     }
+
+    private fun startPhoneNumberVerification(phoneNumber: String){
+        val options =PhoneAuthOptions.newBuilder(auth)
+            .setPhoneNumber(phoneNumber)
+            .setTimeout(60L,TimeUnit.SECONDS)
+            .setActivity(this)
+            .setCallbacks(callbacks)
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
+    }
+    private fun verifyPhoneNumberWithCode(verificationId:String?,code:String)
+    {
+        val credential=PhoneAuthProvider.getCredential(verificationId!!,code)
+
+        signInWithPhoneAuthCredential(credential)
+    }
+
+    private fun signInWithPhoneAuthCredential(credential:PhoneAuthCredential){
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) {task ->
+                if(task.isSuccessful){
+                    Log.d(TAG,"signInWithCredential:success")
+
+                    val user=task.result?.user
+                    Toast.makeText(this,"Welcome to the jungle :"+user,Toast.LENGTH_SHORT).show()
+
+                }
+                else
+                {
+                    Log.w(TAG,"signInWithCredential:failure",task.exception)
+                    if(task.exception is FirebaseAuthInvalidCredentialsException){
+
+                    }
+                }
+            }
+    }
+
+    private fun updateUI(user: FirebaseUser? =auth.currentUser)
+    {
+
+    }
+
+    companion object{
+        private const val TAG="MainActivity"
+    }
+
 }
+
+
